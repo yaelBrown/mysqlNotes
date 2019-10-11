c053cff8f9c3a1b735e9ad6cad6ef5c63a80207e
yaelBrown <yaelrbrown@gmail.com>
2019-10-11 08:34:15 -0500

    Added updated notes from yesterday.

--- codeup_lectures/users.sql
+++ codeup_lectures/users.sql
@@ -17,23 +17,22 @@
 
 -- # We will stick to using 'localhost' for the rest of Codeup.
 
- -- User Permissions --
+-- User Permissions --
 
-  -- # Give a user complete access...
-    GRANT ALL ON *.* TO 'billy'@'localhost';
-    -- # (first * is the database and second * is the table)   
+-- # Give a user complete access...
+  GRANT ALL ON *.* TO 'billy'@'localhost';
+  -- # (first * is the database and second * is the table)   
 
-  -- # Give a user access to a single action...
-    GRANT someSqlActionGoesHere ON someDatabaseName.someDatabaseTable TO 'billy'@'localhost';
+-- # Give a user access to a single action...
+  GRANT someSqlActionGoesHere ON someDatabaseName.someDatabaseTable TO 'billy'@'localhost';
 
-  -- # Give a user access to multiple actions to any table of a database...
-    GRANT someSqlActionGoesHere, anotherSqlActionGoesHere, etc. ON someDatabaseName.* TO 'billy'@'localhost';
+-- # Give a user access to multiple actions to any table of a database...
+  GRANT someSqlActionGoesHere, anotherSqlActionGoesHere, etc. ON someDatabaseName.* TO 'billy'@'localhost';
 
-  -- # Give a user all access and the ability to set other user permissions...
-    GRANT ALL ON *.* TO 'billy'@'localhost' WITH GRANT OPTION;
-
+-- # Give a user all access and the ability to set other user permissions...
+  GRANT ALL ON *.* TO 'billy'@'localhost' WITH GRANT OPTION;
 
 -- # -- DROPPING USERS --
 
-  -- # Dropping a user from the MySQL system (CANNOT BE UNDONE so use with caution)...
-    DROP USER 'billy'@'localhost';
+-- # Dropping a user from the MySQL system (CANNOT BE UNDONE so use with caution)...
+  DROP USER 'billy'@'localhost';
